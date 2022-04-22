590fa03ed621f278019fa25068b76cd96e6babff
luisftime <luisftime@gmail.com>
2022-04-22 02:43:55 -0500

    controllers for models and sessions created

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 class Note < ApplicationRecord
   # Associations
+  belongs_to :user
   has_one :cover, dependent: :destroy
 
   # Validations
   validates :title, presence: true, length: { in: 1..50 }
   validates :body, presence: true, length: { maximum: 1000 }
-  validates :color, presence: true, default: '#ffffff'
-
 end
